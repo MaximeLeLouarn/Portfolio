97369b034181d64b84a80eab9baa48df001d004e
Maxime Le Louarn <maxime.lelouarn@gmail.com>
2024-08-29 14:59:28 +0200

    first

---
+++ template-homepage.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: Homepage
+*/
+get_header();
+?>
+
+<!-- Content -->
+<main id="primary" class="site-main">
+
+    <section class="bubblesbgc">
+        <div class="bubbles">
+        <h1>Bonjour, Je suis Maxime, Développeur Web</h1>
+        </div>
+    </section>
+
+</main>
+
+<?php
+get_sidebar();
+get_footer();
+?>
