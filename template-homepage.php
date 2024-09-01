54053c355dc0025511c7018ce27c142a17293410
Maxime Le Louarn <maxime.lelouarn@gmail.com>
2024-09-01 15:36:07 +0200

    great scallops added

--- template-homepage.php
+++ template-homepage.php
@@ -15,10 +15,16 @@ get_header();
     <section class="infosSkillsContainer">
         <div class="infosSkills">
             <div class="description"> <p>Bonjour, je m'appelle Maxime. En tant que développeur Web et Webmaster, je vous 
-                propose mes services pour vous créer des solutions numériques, ou vous accompagner dans les votres.</p>
+                propose mes services pour vous créer des solutions numériques, ou vous accompagner dans les votres.</p><br>
+                <p class="strong">J'utilise, et peux vous accompagner sur les technologies et les aspects suivants :</p>
             </div>
             <div class="skills">
                 <p>PHP, JS, CSS/SCSS, Backend de WP, SEO, Traduction de site en anglais pour faire du multilingue, Green IT, méthode agile.</p>
+                <div class="skill1">
+                    <img src="<?= get_template_directory_uri() . '/assets/JSscallop.png' ?>" alt="Javascript icon inside a great scallop">
+                    <div class="openedScallop"></div>
+                </div>
+                
             </div>
         </div>
     </section>
